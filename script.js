d2e92bfa8eb75f3573a0f2ec8f77c00272d46929
Moein0Mirsaeedi <Moein.Mirsaeedi@gmail.com>
2024-12-13 13:55:35 +0330

    change theme button

--- script.js
+++ script.js
@@ -21,3 +21,28 @@ helpButton.addEventListener("click", function()
         helpIcon.style.transform = "rotate(0deg)"
     }
 })
+
+
+let changeTheme = document.getElementById("changeTheme");
+let spanTheme = document.getElementById("spanTheme");
+let body = document.body;
+let theme = "light"
+
+function setTheme(theme){
+    if(theme == "dark"){
+        body.classList.toggle("dark");
+    }else if(theme == "light"){
+        body.classList.toggle("dark");
+    }
+}
+
+changeTheme.addEventListener("click", function(){
+    if(theme == "light"){
+        theme = "dark"
+        setTheme("dark");
+    }else if(theme == "dark"){
+        theme = "light"
+        setTheme("light");
+
+    }
+})
