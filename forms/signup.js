e984217c481f9d13162cf30919edb421bb15b904
Moein0Mirsaeedi <Moein.Mirsaeedi@gmail.com>
2024-12-17 20:34:32 +0330

    complete theme

--- forms/signup.js
+++ forms/signup.js
@@ -3,11 +3,30 @@ let spanTheme = document.getElementById("spanTheme");
 let body = document.body;
 let theme = "light"
 
+let localTheme = JSON.parse(localStorage.getItem('theme'))
+console.log(localTheme)
+
+function getLocalTheme(){
+    if (localTheme == "dark" || localTheme == "light"){
+        theme = localTheme
+        setTheme(theme)
+    }else{
+        theme = "light";
+        localStorage.setItem('theme', JSON.stringify(theme))
+        setTheme(theme)
+    }
+}
+getLocalTheme()
+
+function setLocalTheme(){
+    localStorage.setItem('theme', JSON.stringify(theme))
+}
+
 function setTheme(theme){
     if(theme == "dark"){
-        body.classList.toggle("dark");
+        body.classList.add("dark");
     }else if(theme == "light"){
-        body.classList.toggle("dark");
+        body.classList.remove("dark");
     }
 }
 
@@ -15,9 +34,10 @@ changeTheme.addEventListener("click", function(){
     if(theme == "light"){
         theme = "dark"
         setTheme("dark");
+        setLocalTheme()
     }else if(theme == "dark"){
         theme = "light"
         setTheme("light");
-
+        setLocalTheme()
     }
 })
--- script.js
+++ script.js
@@ -34,11 +34,30 @@ let spanTheme = document.getElementById("spanTheme");
 let body = document.body;
 let theme = "light"
 
+let localTheme = JSON.parse(localStorage.getItem('theme'))
+console.log(localTheme)
+
+function getLocalTheme(){
+    if (localTheme == "dark" || localTheme == "light"){
+        theme = localTheme
+        setTheme(theme)
+    }else{
+        theme = "light";
+        localStorage.setItem('theme', JSON.stringify(theme))
+        setTheme(theme)
+    }
+}
+getLocalTheme()
+
+function setLocalTheme(){
+    localStorage.setItem('theme', JSON.stringify(theme))
+}
+
 function setTheme(theme){
     if(theme == "dark"){
-        body.classList.toggle("dark");
+        body.classList.add("dark");
     }else if(theme == "light"){
-        body.classList.toggle("dark");
+        body.classList.remove("dark");
     }
 }
 
@@ -46,9 +65,10 @@ changeTheme.addEventListener("click", function(){
     if(theme == "light"){
         theme = "dark"
         setTheme("dark");
+        setLocalTheme()
     }else if(theme == "dark"){
         theme = "light"
         setTheme("light");
-
+        setLocalTheme()
     }
 })
